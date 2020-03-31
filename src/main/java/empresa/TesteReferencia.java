20596b9661993dedb8deab0d42e319fe1d68021c
flpcarvalho <flp_carvalho@hotmail.com>
2020-03-31 18:24:07 -0300

    poliformismo

--- src/main/java/empresa/TesteReferencia.java
+++ src/main/java/empresa/TesteReferencia.java
@@ -2,16 +2,24 @@ package empresa;
 
 public class TesteReferencia {
     public static void main(String[] args) {
-        Gerente g1 = new Gerente();
+        Funcionario g1 = new Gerente();
         g1.setNome("Felipe");
         g1.setSalario(5000.0);
 
         Funcionario f = new Funcionario();
         f.setSalario(2000.0);
 
+        Funcionario ev = new EditorVideo();
+        f.setSalario(2500.0);
+
+        Funcionario d = new Designer();
+        d.setSalario(3500.0);
+
         ControleBonificacao controle = new ControleBonificacao();
         controle.registra(g1);
         controle.registra(f);
+        controle.registra(ev);
+        controle.registra(d);
 
         System.out.println(controle.getSoma());
 
